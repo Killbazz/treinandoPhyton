282b0ca10c3a9e39b60d0dcae8f7298bf20bfa2c
Marlon Bassoto <corretor.bassoto@gmail.com>
2023-03-28 13:20:19 -0300

    manipulando datas com py

--- Cap. 03/datas_start.py
+++ Cap. 03/datas_start.py
@@ -2,3 +2,23 @@
 # Arquivo com exemplos de manipulação de  datas
 #
 
+from datetime import date
+from datetime import time
+from datetime import datetime
+
+def ManipulaDataHora():
+    hoje = date.today()
+    print ("Hoje é:" , hoje)
+    print ("Partes da Data: " , hoje.day, hoje.month, hoje.year)
+    print ("Numero do dia da Semana: " , hoje.weekday())
+    dias = ["seg", "ter", "qua", "qui", "sex", "sab", "dom"]
+    print("Nome abreviado do dia da semana: " , dias[hoje.weekday()])
+
+    data=datetime.now()
+    print ("Data e Hora: " , data)
+
+    tempo = datetime.time(data)
+    print ("Hora Atual: " , tempo)
+
+    ManipulaDataHora()
+
