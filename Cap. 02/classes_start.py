ab3d57b4eea17d8bd2c5dd7dabc6bf2a7378adfa
Marlon Bassoto <corretor.bassoto@gmail.com>
2023-03-28 12:09:59 -0300

    Criando Classes no PY

--- Cap. 02/classes_start.py
+++ Cap. 02/classes_start.py
@@ -1,3 +1,24 @@
 #
 # Exemplo de como criar classes
 #
+
+class minhaClasse():
+    def __init__(self):
+        self.meuAtributo = "Passou Pelo Construtor!"
+
+    def meuMetodo(self):
+        print("Passou Pelo meuMetodo")
+
+    def meuMetodo2(self, valor):
+        self.outroAtributo = valor
+
+def criaObjeto():
+    meuObj = minhaClasse()
+    var1 = meuObj.meuAtributo
+    print(var1)
+
+    meuObj.meuMetodo()
+
+    meuObj.meuMetodo2("Executando um método")
+
+criaObjeto()
